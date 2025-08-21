0d7442c7b6db1311d26436770ca7e222b679bcab
CMU02 <Hyeonjun1968@naver.com>
2025-08-21 15:45:14 +0900

    feat: Health 체크 엔드포인트 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('health')
+  healthCheck(): { status: string; timestamp: string } {
+    return {
+      status: "OK",
+      timestamp: new Date().toISOString()
+    };
   }
 }
