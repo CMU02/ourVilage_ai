48ab86d843cd4d0a9b7cac5d097010a800d33c96
CMU02 <Hyeonjun1968@naver.com>
2025-08-22 12:53:49 +0900

    refactor: CORS 허용

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,19 @@ async function bootstrap() {
   console.log(process.env.DATABASE_HOST);
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter()); // 전역 필터 등록
+  
+  // CORS 설정
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://ourvliage.cieloblu.co.kr',
+      'http://ourvliage.cieloblu.co.kr'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
